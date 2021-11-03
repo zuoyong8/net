58f93aeedae401115820cef297a17643ac9374f7
zorro <251651680@qq.com>
2021-11-03 15:49:46 +0800

    增加post请求

--- core/xhttp/http.go
+++ core/xhttp/http.go
@@ -1,15 +1,54 @@
 package xhttp
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	jsoniter "github.com/json-iterator/go"
 )
 
+//
 //获取数据
 func Get(url string, headers map[string]string, querys map[string]interface{}) ([]byte, error) {
 	url = joinQuery(url, querys)
 	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	transport := http.Transport{
+		DisableKeepAlives: true, //禁止
+	}
+
+	client := http.Client{
+		Transport: &transport,
+	}
+
+	//
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	info, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
+}
+
+//
+//post
+func Post(url string, headers map[string]string, params map[string]interface{}) ([]byte, error) {
+	ioReder := getBytesReader(params)
+	req, err := http.NewRequest(http.MethodPost, url, ioReder)
 	if err != nil {
 		return nil, err
 	}
@@ -53,4 +92,15 @@ func joinQuery(url string, querys map[string]interface{}) string {
 		}
 	}
 	return url
+}
+
+//
+//
+func getBytesReader(params map[string]interface{}) *bytes.Reader {
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+
+	if bs, err := json.Marshal(params); err == nil && bs != nil {
+		return bytes.NewReader(bs)
+	}
+	return nil
 }
--- core/xhttp/http_test.go
+++ core/xhttp/http_test.go
@@ -12,11 +12,27 @@ func TestHttp(t *testing.T) {
 	// 	"s": "b",
 	// })
 
-	q := map[string]interface{}{
-		"symbol": "BTCUSDT",
-		"limit":  60,
-	}
+	// h := map[string]string{
+	// 	"Content-Type": "application/json",
+	// 	"Language":     "zh-CN",
+	// }
+	// q := map[string]interface{}{
+	// 	"phone":    "15221552956",
+	// 	"areaCode": "0086",
+	// 	"areaFlag": "12333",
+	// 	"smsCode":  "6669",
+	// 	"device":   2,
+	// }
+	// bs, err := Post("https://api.wikifxpay.com/api/v1/user/quick/login", h, q)
+	// fmt.Println(err, string(bs))
 
-	s, _ := Get("https://q.wikibit.us/api/v3/trades", nil, q)
-	fmt.Println(string(s))
+	s, a := JsonToString("http://rating.fx696.com/eahost/ext/eabrokers.json", "brokerid")
+	fmt.Println(s, a)
+
+	// var mm map[string]int = make(map[string]int, 0)
+	// for k, v := range s {
+	// 	mm[v] = k + 1
+	// 	fmt.Println(k, v)
+	// }
+	// fmt.Println(mm["3351410785"])
 }
